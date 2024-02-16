eee5a18695e9e9a35c77e66586e70d732e47b374
Blake Romrell <blakepromrell@gmail.com>
2024-02-16 10:33:22 -0700

    create file for GMRES

---
+++ GMRES.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+class gmres:
+    def __init__(self):
+        pass
+
+    def mygmres(
+        self, l: int, b: np.ndarray, x0: np.ndarray, n: int, M: np.ndarray, A: np.array
+    ):
+        # l: number of iterations
+        # b: right hand side
+        # x0: initial guess
+        # n: number of unknowns
+        # M: preconditioner
+        # A: matrix
+
+        pass
+        # TODO: Implement GMRES
