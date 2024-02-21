bc41fd64509ec8f4894f0d9bdaa7885224fea681
Blake Romrell <blakepromrell@gmail.com>
2024-02-21 13:05:26 -0700

    created a file with blank functions to fill in for generating matrices A and B

---
+++ MatGenerator.py
@@ -0,0 +1,16 @@
+import numpy as np
+import scipy.sparse as sp
+
+
+def generateA(n: int, gamma: float) -> sp.csr_matrix:
+    # n: points in the grid
+    # gamma: parameter
+    # returns: a sparse matrix
+    pass
+
+
+def generateB(n: int, gamma: float) -> np.ndarray:
+    # n: points in the grid
+    # gamma: parameter
+    # returns: a nx1 np array
+    pass
